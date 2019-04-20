30487824028fba46d6d6c61dc959f9bc90000e38
taurusjun@hotmail.com <taurusjun@hotmail.com>
2019-04-21 00:31:26 +0800

    add uwsgi

---
+++ foobar.py
@@ -0,0 +1,4 @@
+# vim foobar.py
+def application(env,start_response):
+    start_response('200 ok',[('Content-Type','text/html')])
+    return [b"Hello World"]
--- jwsit/settings.py
+++ jwsit/settings.py
@@ -10,9 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-# import pymysql
-# pymysql.install_as_MySQLdb()
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
